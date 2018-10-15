3ea5956403efb24588f0340018d3149ca3ed5e4b
Dejan Bavdaz <dejan.bavdaz@elev.ga.lbs.se>
2018-10-15 11:09:18 +0200

    första dagen för kolition

---
+++ Assets/RotatingWall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingWall : MonoBehaviour
+{
+    public float xspeed;
+    public float zspeed;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, 0, zspeed * Time.deltaTime);
+        transform.Rotate(xspeed * Time.deltaTime, 0, 0);
+
+    }
+}
---
+++ Assets/movment.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class movment : MonoBehaviour
+{
+    public Rigidbody2D rbody;
+    public float force = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //AddForce
+        //.velocety
+        //( MovePosition() )
+
+        //AddTorque
+        //.angularVelocity
+        //( MoveTotation() )
+    }
+
+    void FixedUpdate()
+    {
+        // startar spelet på Level0 om man trycker på R
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level0");
+        }
+        // ger Movement till player
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+        rbody.AddForce(new Vector2(force, 0f), ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rbody.AddForce(new Vector2(-force, 0f));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rbody.AddForce(new Vector2(force, 0f));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rbody.AddForce(new Vector2(0f, force));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rbody.AddForce(new Vector2(0f, -force));
+        }
+
+    }
+    // Enter
+    // Exit
+    // Stay
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Hit!");
+        SceneManager.LoadScene("Level0");
+    }
+}
